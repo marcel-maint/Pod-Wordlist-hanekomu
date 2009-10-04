6229084ca3043d12274b903e07db1670f3df2fc6
Marcel Gruenauer <hanekomu@gmail.com>
2009-10-04 16:25:48 +0200

    added words; releng

--- lib/Pod/Wordlist/hanekomu.pm
+++ lib/Pod/Wordlist/hanekomu.pm
@@ -1,105 +1,104 @@
 package Pod::Wordlist::hanekomu;
-
+use 5.006;
 use strict;
 use warnings;
-use 5.006;
 use Test::Spelling;
 use utf8;
-
-our $VERSION = '0.01';
-
+our $VERSION      = '0.01';
 our @person_names = qw(
-    Achim
-    Adam
-    Conway
-    Damian
-    Eilam
-    Ekker
-    Florian
-    Grünauer
-    Heinz
-    Helmberger
-    Hofstetter
-    Marcel
-    Mark
-    Miyagawa
-    Ran
-    Takesako
-    Tatsuhiko
+  Achim
+  Adam
+  Conway
+  Damian
+  Eilam
+  Ekker
+  Florian
+  Grünauer
+  Heinz
+  Helmberger
+  Hofstetter
+  Marcel
+  Mark
+  Miyagawa
+  Ran
+  Takesako
+  Tatsuhiko
 );
-
 our @tech_names = qw(
-    AOP
-    API
-    AspectJ
-    CPAN
-    Coro
-    DateTime
-    DBI
-    EPP
-    FirePHP
-    Firefox
-    MVC
-    Middleware
-    OO
-    OOP
-    PARC
-    PHP
-    ShipIt
-    Spiffy
-    W3CDTF
-    YAML
+  AOP
+  API
+  AspectJ
+  CPAN
+  Coro
+  DateTime
+  DBI
+  EPP
+  FirePHP
+  Firefox
+  MVC
+  Markdown
+  mkdn
+  Middleware
+  OO
+  OOP
+  PARC
+  PHP
+  ShipIt
+  Spiffy
+  W3CDTF
+  YAML
 );
-
 our @stopwords = qw(
-    administrativa
-    behaviour
-    blog
-    blogs
-    bugtracker
-    callback
-    callbacks
-    chomp
-    chomps
-    configurator
-    configurators
-    crosscutting
-    distname
-    github
-    homepage
-    init
-    monkeypatch
-    monkeypatches
-    monkeypatching
-    op
-    pipe's
-    placeholders
-    plugins
-    pointcut
-    pointcuts
-    redispatch
-    ref
-    san
-    segment's
-    shipit
-    stopword
-    stopwords
-    tokenizes
-    unshifts
-    username
-    whitelist
-    whitelists
-    wormhole
-    yml
+  administrativa
+  behaviour
+  blog
+  blogs
+  bugtracker
+  callback
+  callbacks
+  chomp
+  chomps
+  configurator
+  configurators
+  crosscutting
+  distname
+  github
+  homepage
+  init
+  mixin
+  monkeypatch
+  monkeypatches
+  monkeypatching
+  ok
+  op
+  pipe's
+  placeholders
+  plugins
+  pointcut
+  pointcuts
+  redispatch
+  ref
+  san
+  segment's
+  shipit
+  stopword
+  stopwords
+  stringifies
+  tokenizes
+  unshifts
+  username
+  uuid
+  whitelist
+  whitelists
+  wormhole
+  yml
 );
 
 # dedup
 my %words = map { $_ => 1 } @person_names, @tech_names, @stopwords;
 add_stopwords(sort keys %words);
 $ENV{LANG} = 'C';
-
 1;
-
 __END__
 
 =head1 NAME
@@ -132,7 +131,7 @@ See perlmodinstall for information and options on installing Perl modules.
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+site near you. Or see L<http://search.cpan.org/dist/Pod-Wordlist-hanekomu/>.
 
 The development version lives at L<http://github.com/hanekomu/pod-wordlist-hanekomu/>.
 Instead of sending patches, please fork this project using the standard git
